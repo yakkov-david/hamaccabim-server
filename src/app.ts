1ddb31d68910476aac997826464b86aca769d303
Yakkov david <yakkovdavid77@gmail.com>
2024-08-25 00:53:19 +0300

    changes

--- src/app.ts
+++ src/app.ts
@@ -148,7 +148,7 @@ const app: Application = express(feathers());
 app.configure(configuration(configurationValidator));
 
 
-/*app.use(cors({
+app.use(cors({
   origin: function (origin, callback) {
       const allowedOrigins = ['http://localhost:3000', 'https://hamaccabim.netlify.app/'];
       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
@@ -160,7 +160,7 @@ app.configure(configuration(configurationValidator));
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'], // Allowed headers
   credentials: true // Allow cookies and other credentials to be sent
-}));*/
+}));
 
 
 // Middleware to handle preflight requests for CORS
